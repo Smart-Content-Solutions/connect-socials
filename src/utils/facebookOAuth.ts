1afd359d3fc30b89a5864591554570cd04886315
extremecoder-rgb <hsuswiowkskow@gmail.com>
2025-11-21 16:39:46 +0530

    fixed linkedin

--- src/utils/facebookOAuth.ts
+++ src/utils/facebookOAuth.ts
@@ -27,7 +27,7 @@ const N8N_URL = import.meta.env.VITE_N8N_WEBHOOK_URL;
 const FACEBOOK_CLIENT_ID = import.meta.env.VITE_FACEBOOK_CLIENT_ID;
 
 // FINAL redirect (frontend)
-const REDIRECT_URI = "https://www.smartcontentsolutions.co.uk/facebook/callback";
+const REDIRECT_URI = "https://smartcontentsolutions.co.uk/facebook/callback";
 
 // Generate random state
 function generateState(length = 32) {
