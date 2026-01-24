d6628bc30068219e37df146328d04642edb2018c
extremecoder-rgb <hsuswiowkskow@gmail.com>
2026-01-24 17:48:42 +0530

    useref error fix 1

--- src/components/apps/SocialAutomationApp.tsx
+++ src/components/apps/SocialAutomationApp.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useCallback } from "react";
+import React, { useState, useRef, useCallback, useEffect } from "react";
 import { toast } from "sonner";
 
 import { useUser } from "@clerk/clerk-react";
