5f55d6cdd2da7a165a665f73871c280998a098d6
extremecoder-rgb <hsuswiowkskow@gmail.com>
2026-03-29 18:52:07 +0530

    clerk twice auth code fix1

--- src/pages/base44/Login.tsx
+++ src/pages/base44/Login.tsx
@@ -4,8 +4,6 @@ export default function Login() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 p-6">
       <SignIn
-        routing="path"
-        path="/login"           // <-- REQUIRED!!!
         signUpUrl="/sign-up"
         fallbackRedirectUrl="/home"
         appearance={{
--- src/pages/base44/Signup.tsx
+++ src/pages/base44/Signup.tsx
@@ -4,8 +4,6 @@ export default function SignupPage() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-50 p-4">
       <SignUp
-        path="/sign-up"
-        routing="path"
         signInUrl="/login"
         fallbackRedirectUrl="/home"
       />
